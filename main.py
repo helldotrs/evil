cc5e84cbbde6c1d02c4e223bd8c8d6f239190c66
Rebekah <macrosystem@proton.me>
2024-03-24 02:44:33 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def ch001_beginnings():
+    print("you wake up")
+    print("it is cold")
+    print("your eyes feels wont open")
+    print("your hands are covered in what appers to be mud"
+
+
+def main()
+  ch001_beginnings()
+
+main()

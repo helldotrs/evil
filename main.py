79b18b674b32f3b46d05fd98547df09458454385
Rebekah <macrosystem@proton.me>
2024-04-21 22:11:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ eyes.open    = False
 eyes.locked  = True
 body.pos     = "lying"
 
+#actions
+look = "you cant see"
+feel_ground = "it feels like mud"
+feel_eyes = "your eyes seem to be shut"
+
 def ch001_beginnings():
     print("you wake up")
     print("it is cold")
-    print("your eyes feels wont open")
-    print("you feel like your hands are covered in what seem to be mud")
+
+    
 
 
 def main()
